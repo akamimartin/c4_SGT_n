9684c9e968d487c12c73fc23690109513d98e951
Shawn Otomo <kodama78@gmail.com>
2015-05-14 16:11:43 -0700

    closes issue #26

---
+++ main.js
@@ -0,0 +1,9 @@
+var student_array = [];
+var average = 0;
+function average_grade(){
+	var total = 0;
+	for (var i = 0; i < student_array.length; i++){
+		total += student_array[i].grade;
+	}
+	average = total/student_array.length;
+}
