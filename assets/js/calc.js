491d9e237edf443c3e924dc3b2031e0a1f4b4aa2
Scott Bowler <s.bowler1007@gmail.com>
2015-05-14 16:26:56 -0700

    corrected highlight function

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -18,19 +18,22 @@ function displayAvg(){
 
 function highLowHighlight(){
 	var row = $(".student-row");
-	var loop = row.length;
-	var high = -1;
-	var low = 101;
-
+	var loop = studentArray.length;
+	var highIndex = 0; 
+	var lowIndex = 0;
 	for(var i=0; i<loop; i++){
-		if(studentArray[i] > studentArray[high]){
-			high = i;
-		}else if(studentArray[i] < studentArray[low]){
-			low = i;
+
+		if(studentArray[i].grade > studentArray[high].grade){
+			highIndex = i;
+		}else if(studentArray[i].grade < studentArray[low].grade){
+			lowIndex = i;
 		}
 	}
 
-	if(high > -1 && low < 100 && high != low){	
+	row.removeClass("high");
+	row.removeClass("low");
+	
+	if(highIndex != 0 && lowIndex != 0 && highIndex != lowIndex){	
 		row(high).addClass("high");
 		row(low).addClass("low");
 	}else{
