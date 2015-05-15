6a8d76c9ff2bb6c693e2a76fabdcde25be2443c1
mkidushim <mkidushim@gmail.com>
2015-05-15 16:12:40 -0700

    4:15pm working on for loop

--- student.js
+++ student.js
@@ -9,17 +9,41 @@ var student_object = {
 
 
 function add_student() {
-    var students = 'Name: ' + $('#student_name').val() + ' | Course: ' + $('#student_course').val() + ' | Grade: ' + $('#student_grade').val();
+
     var addstudent = Object.create(student_object);
     addstudent.name = $('#student_name').val();
     addstudent.course = $('#student_course').val();
     addstudent.grade = $('#student_grade').val();
     student_array.push(addstudent);
-    $('#student_object').val(students);
+
     console.log(student_array)
 }
+
+function show_student() {
+    console.log('button worked')
+    var output_stud = $('<div class="student_info col-md-12">');
+    var name_o = $('<div class="s_name col-md-6">');
+    var course_o = $('<div class="s_course col-md-2">');
+    var grade_o = $('<div class="s_grade col-md-2">');
+    var delete_o = $('<button type="button" class="col-md-2">delete</button>');
+
+    $('#student_object').append(output_stud);
+    $(output_stud).append(name_o, course_o, grade_o, delete_o);
+
+    for (var i = 0; i < 6; i++) {
+
+        $('.s_name').text(student_array[i]['name'])
+        $('.s_course').text(student_array[i].course)
+        $('.s_grade').text(student_array[i].grade)
+
+    }
+}
+
+
+
 $(document).ready(function() {
     $('.s_add').on('click', add_student);
+
 
 });
 
