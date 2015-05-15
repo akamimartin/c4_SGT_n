0f4e05c5f162aa1d7ec83ba1053c88c275713fe5
Eric Johnson <ej2586@gmail.com>
2015-05-15 16:05:46 -0700

    fixed row spacing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,56 @@
 //Main JS File
 var studentArray = [];
 
+var formFields = [];
+
 $(document).ready(function(){
+
+	formFields.push($("#student_name"));
+	formFields.push($("#student_course"));
+	formFields.push($("#student_grade"));
 
 	$("#student_input").on("click", "button", function(){
 		if(addStudent()){
 			displayAvg();
 			highLowHighlight();
 		}
-	})
+	});
+
+	updateStudentList();
 
 });
+
+function updateStudentList(){
+	var contentContainer = $('#student_info .panel-body');
+	if(studentArray.length > 0){
+		contentContainer.html('');
+		$.each(studentArray, function(){
+			contentContainer.append(this.elm);
+		});
+	}else{
+		contentContainer.append('<h5 class="col-sm-12 text-center">No Student info</h5>');
+	}
+	
+}
+
+function resetFormValues(){
+	$.each(formFields, function(){
+		this.val('');
+	})
+}
+
+function createRowElement(obj){
+	var row = $('<div>').addClass('student-row row list-group-item');
+	var nameElm = $('<div>').addClass('col-sm-4 col-xs-3').html(obj.name);
+	var courseElm = $('<div>').addClass('col-sm-3 col-xs-3').html(obj.course);
+	var gradeElm = $('<div>').addClass('col-sm-2 col-xs-3').html(obj.grade);
+	var opElm = $('<div>').addClass('col-sm-2 col-xs-2');
+	var delBtnElm = $('<button>').addClass('btn btn-danger btn-sm pull-right	').html('<span class="glyphicon glyphicon-trash" aria-hidden="true"> </span><span class="hidden-xs"> Remove</span>');
+	opElm.append(delBtnElm);
+	row.append(nameElm, courseElm, gradeElm, opElm);
+
+	return row;
+}
 
 function addStudent(){
 	var nameIn = $("#student_name");
@@ -72,19 +112,20 @@ function addStudent(){
 	}
 
 	if(!error){
+		//create student object and push to the studentArray
 		var student = {};
 		student.name = nameValue;
 		student.course = courseValue;
 		student.grade = gradeValue;
 		studentArray.push(student);
-		student = {};
 
-		var row = $("<div class='student_container panel-body'><div class='student-row row'><div class='col-sm-4 col-xs-3'>" + nameValue + "</div><div class='col-sm-3 col-xs-3'>" + courseValue + "</div><div class='col-sm-2 col-xs-3'>" + gradeValue + "</div><div class='col-sm-2 col-xs-2'><button class='btn btn-danger btn-sm'><span class='glyphicon glyphicon-trash' aria-hidden='true'> </span><span class='hidden-xs'>Remove</span></button></div></div></div>");
-		row.appendTo("#student_info");
+		//create Row Element by object
+		student.elm = createRowElement(student);
 
-		nameIn.val('');
-		courseIn.val('');
-		gradeIn.val('');
+		//reset the form values
+		resetFormValues();
+
+		updateStudentList();
 
 		return true;
 	}
