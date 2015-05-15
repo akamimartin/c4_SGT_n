12113bc5ecb44a26f4942a257ac62880534600ab
mkidushim <mkidushim@gmail.com>
2015-05-15 09:58:10 -0700

    added bootstrap

--- student.js
+++ student.js
@@ -5,6 +5,10 @@ var student_object = {
     grade: null
 };
 
+function add_input() {
+    $('#student_object').append();
+}
+
 function add_student() {
     var addstudent = Object.create(student_object);
     addstudent.name = $('#student_name').val();
@@ -12,6 +16,7 @@ function add_student() {
     addstudent.grade = $('#student_grade').val();
     student_array.push(addstudent);
     console.log(student_array)
+    add_input();
 }
 $(document).ready(function() {
     $('.s_add').on('click', add_student);
