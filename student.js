41b475df3ab211c35ff643bbc46a05cb65a08578
mkidushim <mkidushim@gmail.com>
2015-05-16 11:01:32 -0700

    finished issue #35

--- student.js
+++ student.js
@@ -11,37 +11,44 @@ var student_object = {
 function add_student() {
 
     var addstudent = Object.create(student_object);
-    addstudent.name = $('#student_name').val();
-    addstudent.course = $('#student_course').val();
-    addstudent.grade = $('#student_grade').val();
+    addstudent.s_name = $('#student_name').val();
+    addstudent.s_course = $('#student_course').val();
+    addstudent.s_grade = parseFloat($('#student_grade').val());
     student_array.push(addstudent);
-
+    //show_student();
     console.log(student_array)
 }
 
 function show_student() {
     console.log('button worked')
-    var output_stud = $('<div>', {
-        class: "student_info list-group",
-    });
-    var name_o = $('<div>', {
-        class: "s_name list-group-item"
-    });
-    var course_o = $('<div>', {
-        class: "s_course list-group-item"
-    });
-    var grade_o = $('<div>', {
-        class: "s_grade list-group-item"
-    });
-    var delete_o = $('<button>', {
-        class: "list-group-item",
-        type: "button",
-        text: "delete"
-    });
 
-    $('#student_object').append(output_stud);
-    $(output_stud).append(name_o, course_o, grade_o, delete_o);
 
+
+    for (var i = 0; i < student_array.length; i++) {
+        var output_stud = $('<div>', {
+            class: "student_info list-group",
+        });
+        var name_o = $('<div>', {
+            class: "st_name list-group-item",
+            text: student_array[i].s_name
+        });
+        var course_o = $('<div>', {
+            class: "st_course list-group-item",
+            text: student_array[i].s_course
+        });
+        var grade_o = $('<div>', {
+            class: "st_grade list-group-item",
+            text: student_array[i].s_grade
+        });
+        var delete_o = $('<button>', {
+            class: "list-group-item",
+            type: "button",
+            text: "delete"
+        });
+        $('#student_object').append(output_stud);
+        $(output_stud).append(name_o, course_o, grade_o, delete_o);
+
+    }
 
 };
 
